4c2f1c9c07384336b95e2bf2565b4d850c92de77
Blan25ga <gabriel.bl@hotmail.com.ar>
2026-09-24 00:11:56 -0300

    Add project files.

---
+++ PrototipoASP/Direccion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrototipoASP
+{
+    public class Direccion
+    {
+        public int Id { get; set; }
+        public string Calle { get; set; }
+        public int Altura { get; set; }
+
+    }
+}
